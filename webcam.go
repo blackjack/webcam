3f2380afa145a808733022af4dc0ad9800fb0b5b
Oleksandr Senkovych <bjsenya@gmail.com>
2013-09-30 14:28:44 +0300

    Free dev string on function exit

--- webcam.go
+++ webcam.go
@@ -6,6 +6,7 @@ import "unsafe"
 
 func GetImg(path string) []byte {
 	dev := C.CString(path)
+	defer C.free(unsafe.Pointer(dev))
 	var length C.int
 	arr := C.go_get_webcam_frame(dev, &length)
 	return C.GoBytes(unsafe.Pointer(arr), length)
