2f903636e553f27d0cc4d0bf5955f797dfa25223
Andrew McRae <andrew.a.mcrae@gmail.com>
2019-01-18 16:25:48 +1100

    Make RGB processing common.

---
+++ frame/rgb.go
@@ -0,0 +1,80 @@
+package frame
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+)
+
+type fRGB struct {
+	model   color.Model
+	b       image.Rectangle
+	width   int
+	roffs	int
+	goffs	int
+	boffs	int
+	frame   []byte
+	release func()
+}
+
+// Register framers for these formats.
+func init() {
+	RegisterFramer("RGB3", newFramerRGB3)
+	RegisterFramer("BGR3", newFramerBGR3)
+}
+
+// Return a function that is used as a framer for RGB3.
+func newFramerRGB3(w, h int) func([]byte, func()) (Frame, error) {
+	return newRGBFramer(w, h, 0, 1, 2)
+}
+
+// Return a function that is used as a framer for BGR3.
+func newFramerBGR3(w, h int) func([]byte, func()) (Frame, error) {
+	return newRGBFramer(w, h, 2, 1, 0)
+}
+
+// Return a function that is used as a generic RGB framer.
+func newRGBFramer(w, h, r, g, b int) func([]byte, func()) (Frame, error) {
+	var size, bw int
+	if *padded {
+		bw = (w + 31) &^ 31
+		size = 3 * bw * ((h + 15) &^ 15)
+	} else {
+		size = 3 * h * w
+	}
+	return func(buf []byte, rel func()) (Frame, error) {
+		return frameRGB(size, bw, w, h, r, g, b, buf, rel)
+	}
+}
+
+// Wrap a raw webcam frame in a Frame so that it can be used as an image.
+func frameRGB(size, bw, w, h, rof, gof, bof int, b []byte, rel func()) (Frame, error) {
+	if len(b) != size {
+		if rel != nil {
+			defer rel()
+		}
+		return nil, fmt.Errorf("Wrong frame length (exp: %d, read %d)", size, len(b))
+	}
+	return &fRGB{model: color.RGBAModel, b: image.Rect(0, 0, w, h), width: bw,
+		roffs: rof, goffs: gof, boffs: bof, frame: b, release: rel}, nil
+}
+
+func (f *fRGB) ColorModel() color.Model {
+	return f.model
+}
+
+func (f *fRGB) Bounds() image.Rectangle {
+	return f.b
+}
+
+func (f *fRGB) At(x, y int) color.Color {
+	i := f.width*y*3 + x*3
+	return color.RGBA{f.frame[i+f.roffs], f.frame[i+f.goffs], f.frame[i+f.boffs], 0xFF}
+}
+
+// Done with frame, release back to camera (if required).
+func (f *fRGB) Release() {
+	if f.release != nil {
+		f.release()
+	}
+}
