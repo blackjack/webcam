e665af2a2591ca093e9fcdb1c3466046e3e15050
Andrew McRae <amcrae@google.com>
2019-03-30 07:22:31 +1100

    Use stride for line width.

--- yuyv422.go
+++ yuyv422.go
@@ -1,7 +1,6 @@
 package frame
 
 import (
-	"flag"
 	"fmt"
 	"image"
 	"image/color"
@@ -11,40 +10,31 @@ import (
 type fYUYV422 struct {
 	model   color.Model
 	b       image.Rectangle
-	width   int
+	stride  int
 	frame   []byte
 	release func()
 }
-
-var padded = flag.Bool("padded", false, "Frame has padding")
 
 // Register a framer factory for this format.
 func init() {
 	RegisterFramer("YUYV", newFramerYUYV422)
 }
 
-func newFramerYUYV422(w, h int) func([]byte, func()) (Frame, error) {
-	var size, bw int
-	if *padded {
-		bw = (w + 31) &^ 31
-		size = 2 * bw * ((h + 15) &^ 15)
-	} else {
-		size = 2 * h * w
-	}
+func newFramerYUYV422(w, h, stride int) func([]byte, func()) (Frame, error) {
 	return func(b []byte, rel func()) (Frame, error) {
-		return frameYUYV422(size, bw, w, h, b, rel)
+		return frameYUYV422(h * stride, stride, w, h, b, rel)
 	}
 }
 
 // Wrap a raw webcam frame in a Frame so that it can be used as an image.
-func frameYUYV422(size, bw, w, h int, b []byte, rel func()) (Frame, error) {
+func frameYUYV422(size, stride, w, h int, b []byte, rel func()) (Frame, error) {
 	if len(b) != size {
 		if rel != nil {
 			defer rel()
 		}
 		return nil, fmt.Errorf("Wrong frame length (exp: %d, read %d)", size, len(b))
 	}
-	f := &fYUYV422{model: color.YCbCrModel, b: image.Rect(0, 0, w, h), width: bw, frame: b, release: rel}
+	f := &fYUYV422{model: color.YCbCrModel, b: image.Rect(0, 0, w, h), stride: stride, frame: b, release: rel}
 	runtime.SetFinalizer(f, func(obj Frame) {
 		obj.Release()
 	})
@@ -60,7 +50,7 @@ func (f *fYUYV422) Bounds() image.Rectangle {
 }
 
 func (f *fYUYV422) At(x, y int) color.Color {
-	index := f.width*y*2 + (x&^1)*2
+	index := f.stride*y + (x&^1)*2
 	if x&1 == 0 {
 		return color.YCbCr{f.frame[index], f.frame[index+1], f.frame[index+3]}
 	} else {
